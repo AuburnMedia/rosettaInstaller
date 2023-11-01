fce77ca1b6e691b23e34dee627b800609a9d6579
AuburnMedia <nicholas.houghton@smseymour.catholic.edu.au>
2023-11-01 15:11:31 +1100

    Bugged, partial fix for mkfile err

--- bin/installer.sh
+++ bin/installer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+homedir=$(eval echo "~/")
 print_line_by_line() {
     local text="$1"
     local length=${#text}
@@ -32,7 +34,8 @@ draw_progress_bar() {
     local space=$(printf "%0.s " $(seq $((width - progress))))
     
     printf "\r[%-${width}s] %d%%" "$bar$space" "$percentage"
-} 
+}
+
 
 create_file() {
   local name=$1
@@ -44,24 +47,53 @@ create_file() {
     return 1
   fi
 
-  mkfile -n "${size}m" "${location}/${name}"
+  local file_path="${location}/${name}"
+
+  # Ensure the parent directory exists
+  create_folder "$location"  
+
+  mkfile -n "${size}m" "$file_path"
+}
+
+
+create_folder() {
+  local location="$1"
+  local expanded_location
+
+  
+  expanded_location=$(eval echo "$location")
+
+  if [[ -d "$expanded_location" ]]; then
+    printf "\n"
+  else
+    mkdir -p "$expanded_location"
+  fi
 }
 
 printf "\033[2J\033[H"
 printf "\n\n\n"
 print_rosetta_installer
 sleep 2
+
+/bin/echo -n "Are you sure you want to continue? (y/n) : "
+read answer
+
+create_folder "~/Library/Caches/rosettaInstaller"
+
 printf "\n\n"
 printf "Installing..."
 sleep 1
 
 
 for i in {1..50}; do
+    clear
     draw_progress_bar $i
     sleep 0.1
-    random_number = $(shuf -i 1-3 -n 1)
-    random_string=$(LC_ALL=C tr -dc 'a-zA-Z' < /dev/urandom | head -c 6)
-    create_file "$random_name" "$random_number" "~/Library/Caches/rosettaInstaller"
+    clear
+    random_number=$(jot -r 1 1 3)
+    random_string=$(openssl rand -base64 12)
+    create_file "$random_string" "$random_number" "${homedir}/Library/Caches/rosettaInstaller"
+    clear
 done
 
 echo "Waiting...."
